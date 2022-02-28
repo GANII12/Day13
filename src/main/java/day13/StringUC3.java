be4dd353214e058eb1bfd2f4f7672c5e0d2dd86f
GANII12 <ganesh.17je003174@ece.ism.ac.in>
2022-02-28 13:20:26 +0530

    adding

---
+++ src/main/java/day13/StringUC3.java
@@ -0,0 +1,15 @@
+package day13;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class StringUC3 {
+    @Test
+    public void givenMaximumStringAt1stPositionForStringsReturnTheSameString(){
+        String a = "Apple";
+        String b = "Peach";
+        String c = "Banana";
+
+    }
+
+}
